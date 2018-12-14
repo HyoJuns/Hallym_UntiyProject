24963d03672e4a57a9d0c34e0b7ce8a0c28ca38a
HyoJuns <43162514+HyoJuns@users.noreply.github.com>
2018-12-14 23:38:57 +0900

    Add files via upload

--- Script/Cs_Spawn.cs
+++ Script/Cs_Spawn.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Cs_Spawn : MonoBehaviour {
 
+
+	public static int num;
 	public GameObject obj;		// 내 자신
-	public float delay = 10.0f;
+	public float delay;
+	private float maxdelay;
 	public Cs_EnemyManager manager;
 	public Enemy e;
-
+	private void Start() {
+		
+		maxdelay = delay;
+	}
 	private void Update() {
 		delay -= Time.deltaTime;
+		
+		GameObject[] n = GameObject.FindGameObjectsWithTag("enemy");
 
-		if(delay <= 0.4f)
+		num = n.Length;
+		if(delay <= 0.4f && num <= 100)
 		{
 			// 리스폰
 			manager.SpawnMonster(obj,e);
-			delay = 10.0f;
+			delay = maxdelay;
+			//num ++;
+			//Debug.Log(num);
 		}
+		Debug.Log(num);
 	}
 
 
